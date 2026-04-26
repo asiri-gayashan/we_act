879a280cfeb27e9e664613ac792b3e34d730b455
asiri-gayashan <gayashanx@gmail.com>
2026-04-26 11:18:12 +0530

    feat: create Home component with styling and logo image

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import Logo from "../assets/appstore.png"
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+        <Image source={Logo} style={styles.img}/>
+      <Text style={styles.title}>Page Title</Text>
+      <Text style={{ marginTop: 15 }}>Content of the page</Text>
+
+       
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: "red",
+  },
+  img:{
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+  
+});
