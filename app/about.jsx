2ee29b837b933dc20163fef8a4f7b0e6df1409aa
asiri-gayashan <gayashanx@gmail.com>
2026-06-22 23:04:45 +0530

    refactor: remove styles from About component

--- app/about.jsx
+++ app/about.jsx
@@ -15,20 +15,3 @@ const About = () => {
 };
 
 export default About;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontWeight: "bold",
-    fontSize: 18,
-    color: "red",
-  },
-  link: {
-    textDecorationLine: "underline",
-    color: "blue",
-  },
-});
