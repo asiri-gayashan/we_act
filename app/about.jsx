e93c69d5f8788d64bba7068af9a4238a05bc1dc4
asiri-gayashan <gayashanx@gmail.com>
2026-06-24 23:34:30 +0530

    vcn

---
+++ app/about.jsx
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+
+const About = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Page Title</Text>
+      <Text style={{ marginTop: 15 }}>Content of the page</Text>
+      <Link href="/" style={styles.link}>
+        Back to home
+      </Link>
+    </View>
+  );
+};
+export default About;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: "red",
+  },
+  link: {
+    textDecorationLine: "underline",
+    color: "blue",
+  },
+});
