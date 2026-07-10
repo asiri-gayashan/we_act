34373147a419ccc278efaa7c1f3f193fdb42b4fb
asiri-gayashan <gayashanx@gmail.com>
2026-07-10 23:13:38 +0530

    fix: organize styles in About component

--- app/about.jsx
+++ app/about.jsx
@@ -16,3 +16,24 @@ const About = () => {
 export default About;
 
 
+
+// Styles
+
+const styles = StyleSheet.create({
+  container: {
+   
+    
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: "red",
+  },
+  link: {
+    textDecorationLine: "underline",
+    color: "blue",
+  },
+});
