d7dbbe10589fc527cd717ea2306398278aab9792
asiri-gayashan <gayashanx@gmail.com>
2026-07-20 23:58:27 +0530

    fix: remove About component implementation

--- app/about.jsx
+++ app/about.jsx
@@ -2,19 +2,6 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
 
-const About = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Page Title</Text>
-      <Text style={{ marginTop: 15 }}>Content of the page</Text>
-      <Link href="/" style={styles.link}>
-        Back to home
-      </Link>
-    </View>
-  );
-};
-export default About;
-
 
 
 // Styles
