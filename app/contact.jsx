a23baecfae026a3e11855f576f4f8d8f04dfa0e4
asiri-gayashan <gayashanx@gmail.com>
2026-09-01 23:52:57 +0530

    fix: remove contact component implementation

--- app/contact.jsx
+++ app/contact.jsx
@@ -2,17 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
 
-const contact = () => {
-  return (
-     <View style={styles.container}>
-         <Text style={styles.title}>Contact Us</Text>
-         <Text style={{ marginTop: 15 }}>Content of the page</Text>
-         <Link href="/" style={styles.link}>
-           Back to home
-         </Link>
-       </View>
-  )
-}
+
 
 export default contact
 
