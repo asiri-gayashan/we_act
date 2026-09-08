225b6b18cb6b35a555c9b109ada51e19dc4d7b30
asiri-gayashan <gayashanx@gmail.com>
2026-09-08 23:05:11 +0530

    fix: remove ThemedView component implementation

---
+++ components/ThemedView.jsx
@@ -0,0 +1,12 @@
+import { View } from 'react-native'
+import React from 'react'
+
+const ThemedView = () => {
+  return (
+    <View style>
+      <Text>ThemedView</Text>
+    </View>
+  )
+}
+
+export default ThemedView
